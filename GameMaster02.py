e167f6b648a49f05db520274404c0a1632736485
AerlenTheStout <realtheyobros@gmail.com>
2022-10-08 04:01:19 -0300

    removed some asyco cuz it broke some stuff LOL

--- GameMaster02.py
+++ GameMaster02.py
@@ -57,7 +57,7 @@ async def initalization(requestedDifficulty,client,message):
             rowquantity = DIFFICULTIES[difficulty][1]
             minequantity = DIFFICULTIES[difficulty][2]
             badDifficulty = False
-            await CreateGrid()
+            CreateGrid()
 
         if re.match(requestedDifficulty,difficulty)== False:
             await message.channel.send("Please chose a valid difficulty")
@@ -67,7 +67,7 @@ async def initalization(requestedDifficulty,client,message):
 
     
 
-async def CreateGrid():
+def CreateGrid():
 #create the grid by putting a new list in the gird for each rows and then apeending to each rows to make collumns
     global originGrid
     originGrid = list()
@@ -75,9 +75,9 @@ async def CreateGrid():
         originGrid.append(list())
         for y in range(rowlength):
             originGrid[i].append(0)
-    await placeMines()
+    placeMines()
 
-async def placeMines():
+def placeMines():
 #This is the function that generates the mines
     minesplaced = 0
     while minesplaced < minequantity:
@@ -90,14 +90,14 @@ async def placeMines():
     global plotGrid
     for i in originGrid:
         plotGrid = copy.deepcopy(originGrid)
-    await addNumbersAroundBombs()
+    addNumbersAroundBombs()
 
 #This is the function that generates the numbers around the mines
 
 differences = [[0,-1],[0,1],[-1,0],[-1,-1],[-1,1],[1,0],[1,-1],[1,1]]
 #TODO: remover the spoilers after generation testing is done
 
-async def addNumbersAroundBombs():
+def addNumbersAroundBombs():
     for i in originGrid:
         for y in i:
             if y == -1:
@@ -115,15 +115,15 @@ async def addNumbersAroundBombs():
     #this creats a copy grid with the numbers around the mines
     global userGrid
     userGrid = copy.deepcopy(originGrid)
-    await createUserGrid()
+    createUserGrid()
     
 #create new grid to store the discord spoilers
 
-async def createUserGrid():
+def createUserGrid():
     for i in range(rowquantity):
         for y in range(rowlength):
             userGrid[i][y] = -1
-    await startingSpot()
+    startingSpot()
 
 async def editSentGrid():
     #get rows from cordinates, edit corosponding rows from msg[i]
@@ -137,7 +137,7 @@ async def Dig(X,Y,message,client):
     if userGrid[Y][X] == -1:
         userGrid[Y][X] = originGrid[Y][X]
         if originGrid[Y][X] == 0:
-            await aroundZero(X,Y)
+            aroundZero(X,Y)
 
         await editSentGrid()
 
@@ -146,7 +146,7 @@ async def Dig(X,Y,message,client):
         message.delete
         
     else :
-        client.loop.create_task(message.reply("You have already dug this spot", delete_after=4))
+        await message.reply("You have already dug this spot", delete_after=4)
         sleep(10)
         message.delete
     
@@ -155,17 +155,17 @@ async def Flag(X,Y,message,client):
     if userGrid[Y][X] == -1:
         userGrid[Y][X] = -2
         await editSentGrid()
-        client.loop.create_task(message.reply("You have flagged this spot", delete_after=4))
+        await message.reply("You have flagged this spot", delete_after=4)
         sleep(10)
         message.delete
         
     else :
-        client.loop.create_task(message.reply("You have already flagged this spot", delete_after=4))
+        await message.reply("You have already flagged this spot", delete_after=4)
         sleep(10)
         message.delete
 
 
-async def aroundZero(X,Y):
+def aroundZero(X,Y):
     for z in differences:
         try:
             userGrid[Y+z[0]][X+z[1]] += originGrid[Y][X]    
@@ -176,7 +176,7 @@ async def aroundZero(X,Y):
 #TODO: make it so that if you dig a zero it digs all the zeros around it
 # make it so when givem a cordiante it reveal() the number
 # print the grid with emojis
-async def rowCoordinates(Y):  
+def rowCoordinates(Y):  
     Y = int(Y)
     coordTable = {}
     for i in range(rowquantity):
@@ -188,7 +188,7 @@ async def rowCoordinates(Y):
     
             
 
-async def startingSpot():
+def startingSpot():
 #chose a random X to be the first X to be revealed
     zeropicked = False
     while zeropicked == False:
@@ -198,12 +198,12 @@ async def startingSpot():
         if originGrid[randomrow][randomspot] == 0:
             userGrid[randomrow][randomspot] = originGrid[randomrow][randomspot]
             zeropicked = True
-    await userToEmojiGrid()
+    userToEmojiGrid()
 
 #this takes the grid and swaps out each number for its corresponding string from discordspoilers            
 
 #TODO: improve this function
-async def userToEmojiGrid():
+def userToEmojiGrid():
     global emojiGrid
     emojiGrid = copy.deepcopy(userGrid)
     for i in EMOJIS:
@@ -211,10 +211,10 @@ async def userToEmojiGrid():
             for z in y:
                 if z == i:
                     y[y.index(z)] = EMOJIS[i]
-    await finalPrints()
+    finalPrints()
 
 
-async def finalPrints():
+def finalPrints():
 #this prints the original grid
 #prints the grid with only bombs
     for i in plotGrid:
