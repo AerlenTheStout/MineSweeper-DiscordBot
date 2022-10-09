40712a9019bd54622a65a6504a06e16ea879e24c
bbrdbr <aubreysp2015@gmail.com>
2022-10-09 15:10:22 -0300

    moved differences to the top

--- GameMaster02.py
+++ GameMaster02.py
@@ -39,6 +39,8 @@ ALPHABET = {
 
 ALPHABETEMOJI = [":regional_indicator_a:",":regional_indicator_b:",":regional_indicator_c:",":regional_indicator_d:",":regional_indicator_e:",":regional_indicator_f:",":regional_indicator_g:",":regional_indicator_h:",":regional_indicator_i:",":regional_indicator_j:",":regional_indicator_k:",":regional_indicator_l:",":regional_indicator_m:",":regional_indicator_n:",":regional_indicator_o:",":regional_indicator_p:",":regional_indicator_q:",":regional_indicator_r:",":regional_indicator_s:",":regional_indicator_t:",":regional_indicator_u:",":regional_indicator_v:",":regional_indicator_w:",":regional_indicator_x:",":regional_indicator_y:",":regional_indicator_z:"]
 
+differences = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
 async def initalization(requestedDifficulty,client,message):
 #def initalization(requestedDifficulty):
 #This makes and sets the grid variables acording to the difficulty
@@ -96,8 +98,6 @@ def placeMines():
     addNumbersAroundBombs()
 
 #This is the function that generates the numbers around the mines
-
-differences = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
 def addNumbersAroundBombs():
     for i in originGrid:
